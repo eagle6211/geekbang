cfe9a2b8afa75ea07be4c9e4a6a0d6b4308d67ac
eagle <magic6211@126.com>
2017-12-03 15:38:41 +0800

    update 优化发送邮件内容代码

--- geekbang.py
+++ geekbang.py
@@ -1,4 +1,4 @@
-import requests
+】import requests
 import json
 import send_email
 
@@ -7,8 +7,8 @@ headers = {
     'User_Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
 }
 
-def get_data_from(url):
-
+def deal_sented_content(url):
+    #form data
     data = {
         'q':'',
         't':'0',
@@ -20,16 +20,23 @@ def get_data_from(url):
     response = requests.post(url, headers=headers, data=data)
     data = json.loads(response.text)
     data_lst = data.get("result").get("content")
-    content = ''
+    content_lst = []
+    #处理发送邮件内容
     if data_lst:
-        for index, item in enumerate(data_lst):
-            title = item.get('title')
-            if title.find('：') >=0 :
-                title = str(index+1) +")" + title.split('：')[1].strip()
-            else:
-                title = str(index+1) +")" + title
-            content += title +"\n"+ item.get('url')+"\n"
-        send_email.sendEmail('极客搜索', content)
+        for index, item in enumerate(data_lst, 1):
+            title = item.get('title').split('：')[-1].strip()
+            url = item.get('url')
+            content = '({index}) {title} \n {url} '.format(index=index,
+                                                            title=title,
+                                                            url=url
+                                                            )
+            content_lst.append(centent) 
+         content = '\n'.join(content_lst)
+     return content
+
+def main(url):
+    content = deal_sented_content(url)
+    send_email.sendEmail('极客搜索', content)
 
 if __name__ == '__main__':
-    get_data_from(url)
+    main(url)
